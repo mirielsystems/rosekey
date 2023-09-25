f59273bdb5edd2079fdc2e27daa1b0f9b64a6170
Mar0xy <marie@kaifa.ch>
2023-09-25 21:27:39 +0200

    add: description support for mastodon

--- packages/backend/src/core/entities/UserEntityService.ts
+++ packages/backend/src/core/entities/UserEntityService.ts
@@ -473,6 +473,8 @@ export class UserEntityService implements OnModuleInit {
 			}
 		}
 
+		const mastoapi = opts.userProfile ?? await this.userProfilesRepository.findOneByOrFail({ userId: user.id });
+
 		const followingCount = profile == null ? null :
 			(profile.followingVisibility === 'public') || isMe ? user.followingCount :
 			(profile.followingVisibility === 'followers') && (relation && relation.isFollowing) ? user.followingCount :
@@ -517,6 +519,7 @@ export class UserEntityService implements OnModuleInit {
 			isSilenced: this.roleService.getUserPolicies(user.id).then(r => !r.canPublicNote),
 			speakAsCat: user.speakAsCat ?? false,
 			createdAt: this.idService.parse(user.id).date.toISOString(),
+			description: mastoapi!.description,
 			instance: user.host ? this.federatedInstanceService.federatedInstanceCache.fetch(user.host).then(instance => instance ? {
 				name: instance.name,
 				softwareName: instance.softwareName,
@@ -553,7 +556,6 @@ export class UserEntityService implements OnModuleInit {
 				isSilenced: !policies?.canPublicNote,
 				isLimited: !(policies?.canCreateContent && policies.canUpdateContent && policies.canDeleteContent && policies.canInitiateConversation),
 				isSuspended: user.isSuspended,
-				description: profile!.description,
 				location: profile!.location,
 				birthday: profile!.birthday,
 				listenbrainz: profile!.listenbrainz,
