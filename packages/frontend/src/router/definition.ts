ab2d743fbb5e80e2c655642b2a54db1e5e988995
ありす <master@16439s.jp>
2024-02-25 18:11:00 +0900

    Fix

--- packages/frontend/src/router/definition.ts
+++ packages/frontend/src/router/definition.ts
@@ -181,6 +181,10 @@ const routes: RouteDef[] = [{
 		name: 'profile',
 		component: page(() => import('@/pages/settings/accounts.vue')),
 	}, {
+		path: '/subscription',
+		name: 'subscription',
+		component: page(() => import('./pages/settings/subscription.vue')),
+	}, {
 		path: '/other',
 		name: 'other',
 		component: page(() => import('@/pages/settings/other.vue')),
