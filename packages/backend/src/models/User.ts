tanukey-dev/tanukey

wip(subscription): 諸々修正

packages/backend/src/models/User.ts
@@ -212,8 +212,11 @@ export class MiUser {
 	})
 	public subscriptionStatus: typeof subscriptionStatus[number];
 
-	@Column(id())
-	public subscriptionPlanId: MiSubscriptionPlan['id'];
+	@Column({
+		...id(),
+		nullable: true,
+	})
+	public subscriptionPlanId: MiSubscriptionPlan['id'] | null;
 
 	@Column('varchar', {
 		length: 128, array: true, default: '{}',

mirielsystems/rosekey

packages/backend/src/models/User.ts
@@ -214,8 +214,11 @@ export class MiUser {
 	})
 	public subscriptionStatus: typeof subscriptionStatus[number];
 
-	@Column(id())
-	public subscriptionPlanId: SubscriptionPlan['id'];
+	@Column({
+		...id(),
+		nullable: true,
+	})
+	public subscriptionPlanId: SubscriptionPlan['id'] | null;
 
 	@Column('varchar', {
 		length: 128, array: true, default: '{}',