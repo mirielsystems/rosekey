tanukey-dev/tanukey

wip(subscription): サブスクリプションプランをロールから分離
専用のモジュールで管理するようにする。

packages/backend/src/models/SubscriptionPlan.ts
@@ -1,0 +1,30 @@
+import { Column, Entity, Index, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+import { id } from './util/id.js';
+import { MiRole } from "@/models/Role.js";
+
+@Entity('subscription_plan')
+
+export class MiSubscriptionPlan {
+	@PrimaryColumn(id())
+	public id: string;
+
+	@Column('varchar', {
+		length: 128
+	})
+	public stripePriceId: string | null;
+
+	@Index({ unique: true })
+	@Column(id())
+	public roleId: MiRole['id'];
+
+	@ManyToOne(type => MiRole, {
+		onDelete: 'CASCADE',
+	})
+	@JoinColumn()
+	public role: MiRole | null;
+
+	@Column('boolean', {
+		default: false,
+	})
+	public isArchived: boolean;
+}

packages/backend/src/server/StripeWebhookServerService.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { DI } from '@/di-symbols.js';
 import type { Config } from '@/config.js';
-import type { RolesRepository, UsersRepository, UserProfilesRepository } from '@/models/_.js';
+import type { UsersRepository, UserProfilesRepository, SubscriptionPlansRepository } from '@/models/_.js';
 import { RoleService } from '@/core/RoleService.js';
 import { MetaService } from '@/core/MetaService.js';
 import type { FastifyInstance, FastifyPluginOptions } from "fastify";
@@ -21,8 +21,8 @@ export class StripeWebhookServerService {
 		private usersRepository: UsersRepository,
 		@Inject(DI.usersRepository)
 		private userProfilesRepository: UserProfilesRepository,
-		@Inject(DI.rolesRepository)
-		private rolesRepository: RolesRepository,
+		@Inject(DI.subscriptionPlansRepository)
+		private subscriptionPlansRepository: SubscriptionPlansRepository,
 		private roleService: RoleService,
 		private metaService: MetaService,
 		private loggerService: LoggerService,
@@ -34,11 +34,11 @@ export class StripeWebhookServerService {
 	public createServer(fastify: FastifyInstance, options: FastifyPluginOptions, done: (err?: Error) => void) {
 		fastify.post('/webhook', async (request, reply) => {
 			const instance = await this.metaService.fetch(true);
-			if (!(instance.enableSubscriptions && this.config.stripe?.secretKey)) {
+			if (!(instance.enableSubscriptions || this.config.stripe?.secretKey)) {
 				return reply.code(503);
 			}
 
-			const stripe = new Stripe(this.config.stripe.secretKey);
+			const stripe = new Stripe(this.config.stripe!.secretKey);
 
 			const body = request.rawBody;
 			if (!body) {
@@ -46,7 +46,7 @@ export class StripeWebhookServerService {
 			}
 
 			// Check if webhook signing is configured.
-			if (this.config.stripe.webhookSecret) {
+			if (this.config.stripe?.webhookSecret) {
 				// Retrieve the event by verifying the signature using the raw body and secret.
 				let event;
 				const signature = request.headers['stripe-signature'];
@@ -71,14 +71,14 @@ export class StripeWebhookServerService {
 						const subscribeUser = await this.userProfilesRepository.findOneByOrFail({ stripeCustomerId: customer.id });
 
 						if (!subscribeUser) {
-							return reply.code(404);
+							return reply.code(400);
 						}
 
-						const plan = subscription.items.data[0].plan as Stripe.Plan;
-						const role = await this.rolesRepository.findOneByOrFail({ stripeProductId: plan.product as string });
-						await this.roleService.assign(subscribeUser.userId, role.id);
-						this.usersRepository.update({ id: subscribeUser.userId }, {
+						const subscriptionPlan = await this.subscriptionPlansRepository.findOneByOrFail({ stripePriceId: subscription.items.data[0].plan.id });
+						await this.roleService.assign(subscribeUser.userId, subscriptionPlan.roleId);
+						await this.usersRepository.update({id: subscribeUser.userId}, {
 							subscriptionStatus: subscription.status,
+							subscriptionPlanId: subscriptionPlan.id,
 						});
 
 						return reply.code(200);
@@ -92,17 +92,50 @@ export class StripeWebhookServerService {
 						const subscribeUser = await this.userProfilesRepository.findOneByOrFail({ stripeCustomerId: customer.id });
 
 						if (!subscribeUser) {
-							return reply.code(404);
+							return reply.code(400);
 						}
 
-						const plan = subscription.items.data[0].plan as Stripe.Plan;
-						const role = await this.rolesRepository.findOneByOrFail({ stripeProductId: plan.product as string });
-						await this.roleService.unassign(subscribeUser.userId, role.id);
+						const subscriptionPlan = await this.subscriptionPlansRepository.findOneByOrFail({ stripePriceId: subscription.items.data[0].plan.id });
+						await this.roleService.unassign(subscribeUser.userId, subscriptionPlan.roleId);
 						await this.usersRepository.update({ id: subscribeUser.userId }, {
 							subscriptionStatus: subscription.status,
+							subscriptionPlanId: undefined,
 						});
 
 						return reply.code(200);
+					}
+
+					case 'customer.subscription.updated': {
+						// Update the subscription.
+						const subscription = event.data.object;
+
+						const customer = subscription.customer as Stripe.Customer;
+						const subscribeUser = await this.userProfilesRepository.findOneByOrFail({ stripeCustomerId: customer.id });
+
+						if (!subscribeUser) {
+							return reply.code(400);
+						}
+
+						const subscriptionPlan = await this.subscriptionPlansRepository.findOneByOrFail({ stripePriceId: subscription.items.data[0].plan.id });
+						if (subscriptionPlan.id !== subscribeUser.user!.subscriptionPlanId) {
+							// サブスクリプションプランが変更された場合
+							const oldSubscriptionPlan = await this.subscriptionPlansRepository.findOneByOrFail({ id: subscribeUser.user!.subscriptionPlanId });
+							await this.roleService.unassign(subscribeUser.userId, oldSubscriptionPlan.roleId);
+
+							await this.roleService.assign(subscribeUser.userId, subscriptionPlan.id);
+							await this.usersRepository.update({id: subscribeUser.userId}, {
+								subscriptionStatus: subscription.status,
+								subscriptionPlanId: subscriptionPlan.id,
+							});
+						}
+
+						return reply.code(200);
+					}
+
+					case 'invoice.upcoming': {
+						// TODO サブスクリプションプランがアーカイブ状態になっていないかチェックする。
+						// アーカイブになっている場合は、サブスクリプションをキャンセルする。
+						break;
 					}
 
 					case 'invoice.paid':

packages/backend/src/server/api/endpoints/subscription/create.ts
@@ -1,7 +1,12 @@
 import ms from 'ms';
 import { Stripe } from 'stripe';
 import { Inject, Injectable } from '@nestjs/common';
-import type { UsersRepository, UserProfilesRepository, RolesRepository } from '@/models/_.js';
+import type {
+	UsersRepository,
+	UserProfilesRepository,
+	RolesRepository,
+	SubscriptionPlansRepository
+} from '@/models/_.js';
 import { Endpoint } from '@/server/api/endpoint-base.js';
 import { DI } from '@/di-symbols.js';
 import { RoleService } from '@/core/RoleService.js';
@@ -28,16 +33,10 @@ export const meta = {
 			id: '6a27f458-92aa-4807-bbc3-3b8223a84a7e',
 		},
 
-		noSuchRole: {
-			message: 'No such role.',
-			code: 'NO_SUCH_ROLE',
-			id: 'eb70323a-df61-4dd4-ad90-89c83c7cf26e',
-		},
-
-		noSuchSubscription: {
-			message: 'No such subscription.',
-			code: 'NO_SUCH_SUBSCRIPTION',
-			id: 'f8b5f1c0-7d1c-4a5b-9d9d-1d4d7e0c8e3a',
+		noSuchPlan: {
+			message: 'No such plan.',
+			code: 'NO_SUCH_PLAN',
+			id: 'd9f0d5c1-0b5b-4b7a-9d2c-1c1c5c6d1d1d',
 		},
 
 		accessDenied: {
@@ -63,9 +62,9 @@ export const meta = {
 export const paramDef = {
 	type: 'object',
 	properties: {
-		roleId: { type: 'string', format: 'misskey:id' },
+		planId: { type: 'string', format: 'misskey:id' },
 	},
-	required: ['roleId'],
+	required: ['planId'],
 } as const;
 
 @Injectable()
@@ -74,21 +73,21 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 		@Inject(DI.config)
 		private config: Config,
 		@Inject(DI.usersRepository)
-		private usersRepository: UsersRepository,
-		@Inject(DI.usersRepository)
 		private userProfilesRepository: UserProfilesRepository,
-		@Inject(DI.rolesRepository)
-		private rolesRepository: RolesRepository,
-		private roleService: RoleService,
+		@Inject(DI.subscriptionPlansRepository)
+		private subscriptionPlanRepository: SubscriptionPlansRepository,
 		private metaService: MetaService,
 	) {
 		super(meta, paramDef, async (ps, me) => {
 			const instance = await this.metaService.fetch(true);
-			if (!(instance.enableSubscriptions && this.config.stripe?.secretKey)) {
+			if (!(instance.enableSubscriptions || this.config.stripe?.secretKey)) {
 				throw new ApiError(meta.errors.unavailable);
 			}
 
-			const stripe = new Stripe(this.config.stripe.secretKey);
+			const plan = await this.subscriptionPlanRepository.findOneBy({ id: ps.planId });
+			if (plan?.isArchived || !plan?.stripePriceId) {
+				throw new ApiError(meta.errors.noSuchPlan);
+			}
 
 			let subscribeUser = await this.userProfilesRepository.findOneBy({ userId: me.id });
 			if (!subscribeUser) {
@@ -98,6 +97,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 				throw new ApiError(meta.errors.requiredEmail);
 			}
 
+			const stripe = new Stripe(this.config.stripe!.secretKey);
 			if (!subscribeUser.stripeCustomerId) {
 				const makeCustomer = await stripe.customers.create({
 					email: subscribeUser.email,
@@ -106,14 +106,6 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 					stripeCustomerId: makeCustomer.id,
 				});
 				subscribeUser = await this.userProfilesRepository.findOneByOrFail({ userId: me.id });
-			}
-
-			const role = await this.rolesRepository.findOneBy({ id: ps.roleId });
-			if (!role) {
-				throw new ApiError(meta.errors.noSuchRole);
-			}
-			if (!role.stripeProductId) {
-				throw new ApiError(meta.errors.noSuchSubscription);
 			}
 
 			const subscriptionStatus = subscribeUser.user!.subscriptionStatus; // null? wtf. really? why? how? when? where? who? what?
@@ -137,7 +129,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 					billing_address_collection: 'auto',
 					line_items: [
 						{
-							price: role.stripeProductId,
+							price: plan.stripePriceId,
 							quantity: 1,
 						},
 					],

mirielsystems/rosekey

packages/backend/src/models/SubscriptionPlan.ts
@@ -1,0 +1,30 @@
+import { Column, Entity, Index, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+import { Role } from "@/models/Role.js";
+import { id } from './util/id.js';
+
+@Entity('subscription_plan')
+
+export class SubscriptionPlan {
+	@PrimaryColumn(id())
+	public id: string;
+
+	@Column('varchar', {
+		length: 128
+	})
+	public stripePriceId: string | null;
+
+	@Index({ unique: true })
+	@Column(id())
+	public roleId: Role['id'];
+
+	@ManyToOne(type => Role, {
+		onDelete: 'CASCADE',
+	})
+	@JoinColumn()
+	public role: Role | null;
+
+	@Column('boolean', {
+		default: false,
+	})
+	public isArchived: boolean;
+}

packages/backend/src/server/StripeWebhookServerService.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { DI } from '@/di-symbols.js';
 import type { Config } from '@/config.js';
-import type { RolesRepository, UsersRepository, UserProfilesRepository } from '@/models/_.js';
+import type { UsersRepository, UserProfilesRepository, SubscriptionPlansRepository } from '@/models/index.js';
 import { RoleService } from '@/core/RoleService.js';
 import { MetaService } from '@/core/MetaService.js';
 import type { FastifyInstance, FastifyPluginOptions } from "fastify";
@@ -21,8 +21,8 @@ export class StripeWebhookServerService {
 		private usersRepository: UsersRepository,
 		@Inject(DI.usersRepository)
 		private userProfilesRepository: UserProfilesRepository,
-		@Inject(DI.rolesRepository)
-		private rolesRepository: RolesRepository,
+		@Inject(DI.subscriptionPlansRepository)
+		private subscriptionPlansRepository: SubscriptionPlansRepository,
 		private roleService: RoleService,
 		private metaService: MetaService,
 		private loggerService: LoggerService,
@@ -34,11 +34,11 @@ export class StripeWebhookServerService {
 	public createServer(fastify: FastifyInstance, options: FastifyPluginOptions, done: (err?: Error) => void) {
 		fastify.post('/webhook', async (request, reply) => {
 			const instance = await this.metaService.fetch(true);
-			if (!(instance.enableSubscriptions && this.config.stripe?.secretKey)) {
+			if (!(instance.enableSubscriptions || this.config.stripe?.secretKey)) {
 				return reply.code(503);
 			}
 
-			const stripe = new Stripe(this.config.stripe.secretKey);
+			const stripe = new Stripe(this.config.stripe!.secretKey);
 
 			const body = request.rawBody;
 			if (!body) {
@@ -46,7 +46,7 @@ export class StripeWebhookServerService {
 			}
 
 			// Check if webhook signing is configured.
-			if (this.config.stripe.webhookSecret) {
+			if (this.config.stripe?.webhookSecret) {
 				// Retrieve the event by verifying the signature using the raw body and secret.
 				let event;
 				const signature = request.headers['stripe-signature'];
@@ -71,14 +71,14 @@ export class StripeWebhookServerService {
 						const subscribeUser = await this.userProfilesRepository.findOneByOrFail({ stripeCustomerId: customer.id });
 
 						if (!subscribeUser) {
-							return reply.code(404);
+							return reply.code(400);
 						}
 
-						const plan = subscription.items.data[0].plan as Stripe.Plan;
-						const role = await this.rolesRepository.findOneByOrFail({ stripeProductId: plan.product as string });
-						await this.roleService.assign(subscribeUser.userId, role.id);
-						this.usersRepository.update({ id: subscribeUser.userId }, {
+						const subscriptionPlan = await this.subscriptionPlansRepository.findOneByOrFail({ stripePriceId: subscription.items.data[0].plan.id });
+						await this.roleService.assign(subscribeUser.userId, subscriptionPlan.roleId);
+						await this.usersRepository.update({id: subscribeUser.userId}, {
 							subscriptionStatus: subscription.status,
+							subscriptionPlanId: subscriptionPlan.id,
 						});
 
 						return reply.code(200);
@@ -92,17 +92,50 @@ export class StripeWebhookServerService {
 						const subscribeUser = await this.userProfilesRepository.findOneByOrFail({ stripeCustomerId: customer.id });
 
 						if (!subscribeUser) {
-							return reply.code(404);
+							return reply.code(400);
 						}
 
-						const plan = subscription.items.data[0].plan as Stripe.Plan;
-						const role = await this.rolesRepository.findOneByOrFail({ stripeProductId: plan.product as string });
-						await this.roleService.unassign(subscribeUser.userId, role.id);
+						const subscriptionPlan = await this.subscriptionPlansRepository.findOneByOrFail({ stripePriceId: subscription.items.data[0].plan.id });
+						await this.roleService.unassign(subscribeUser.userId, subscriptionPlan.roleId);
 						await this.usersRepository.update({ id: subscribeUser.userId }, {
 							subscriptionStatus: subscription.status,
+							subscriptionPlanId: undefined,
 						});
 
 						return reply.code(200);
+					}
+
+					case 'customer.subscription.updated': {
+						// Update the subscription.
+						const subscription = event.data.object;
+
+						const customer = subscription.customer as Stripe.Customer;
+						const subscribeUser = await this.userProfilesRepository.findOneByOrFail({ stripeCustomerId: customer.id });
+
+						if (!subscribeUser) {
+							return reply.code(400);
+						}
+
+						const subscriptionPlan = await this.subscriptionPlansRepository.findOneByOrFail({ stripePriceId: subscription.items.data[0].plan.id });
+						if (subscriptionPlan.id !== subscribeUser.user!.subscriptionPlanId) {
+							// サブスクリプションプランが変更された場合
+							const oldSubscriptionPlan = await this.subscriptionPlansRepository.findOneByOrFail({ id: subscribeUser.user!.subscriptionPlanId });
+							await this.roleService.unassign(subscribeUser.userId, oldSubscriptionPlan.roleId);
+
+							await this.roleService.assign(subscribeUser.userId, subscriptionPlan.id);
+							await this.usersRepository.update({id: subscribeUser.userId}, {
+								subscriptionStatus: subscription.status,
+								subscriptionPlanId: subscriptionPlan.id,
+							});
+						}
+
+						return reply.code(200);
+					}
+
+					case 'invoice.upcoming': {
+						// TODO サブスクリプションプランがアーカイブ状態になっていないかチェックする。
+						// アーカイブになっている場合は、サブスクリプションをキャンセルする。
+						break;
 					}
 
 					case 'invoice.paid':

packages/backend/src/server/api/endpoints/subscription/create.ts
@@ -1,7 +1,12 @@
 import ms from 'ms';
 import { Stripe } from 'stripe';
 import { Inject, Injectable } from '@nestjs/common';
-import type { UsersRepository, UserProfilesRepository, RolesRepository } from '@/models/_.js';
+import type {
+	UsersRepository,
+	UserProfilesRepository,
+	RolesRepository,
+	SubscriptionPlansRepository
+} from '@/models/index.js';
 import { Endpoint } from '@/server/api/endpoint-base.js';
 import { DI } from '@/di-symbols.js';
 import { RoleService } from '@/core/RoleService.js';
@@ -28,16 +33,10 @@ export const meta = {
 			id: '6a27f458-92aa-4807-bbc3-3b8223a84a7e',
 		},
 
-		noSuchRole: {
-			message: 'No such role.',
-			code: 'NO_SUCH_ROLE',
-			id: 'eb70323a-df61-4dd4-ad90-89c83c7cf26e',
-		},
-
-		noSuchSubscription: {
-			message: 'No such subscription.',
-			code: 'NO_SUCH_SUBSCRIPTION',
-			id: 'f8b5f1c0-7d1c-4a5b-9d9d-1d4d7e0c8e3a',
+		noSuchPlan: {
+			message: 'No such plan.',
+			code: 'NO_SUCH_PLAN',
+			id: 'd9f0d5c1-0b5b-4b7a-9d2c-1c1c5c6d1d1d',
 		},
 
 		accessDenied: {
@@ -63,9 +62,9 @@ export const meta = {
 export const paramDef = {
 	type: 'object',
 	properties: {
-		roleId: { type: 'string', format: 'misskey:id' },
+		planId: { type: 'string', format: 'misskey:id' },
 	},
-	required: ['roleId'],
+	required: ['planId'],
 } as const;
 
 @Injectable()
@@ -74,21 +73,21 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 		@Inject(DI.config)
 		private config: Config,
 		@Inject(DI.usersRepository)
-		private usersRepository: UsersRepository,
-		@Inject(DI.usersRepository)
 		private userProfilesRepository: UserProfilesRepository,
-		@Inject(DI.rolesRepository)
-		private rolesRepository: RolesRepository,
-		private roleService: RoleService,
+		@Inject(DI.subscriptionPlansRepository)
+		private subscriptionPlanRepository: SubscriptionPlansRepository,
 		private metaService: MetaService,
 	) {
 		super(meta, paramDef, async (ps, me) => {
 			const instance = await this.metaService.fetch(true);
-			if (!(instance.enableSubscriptions && this.config.stripe?.secretKey)) {
+			if (!(instance.enableSubscriptions || this.config.stripe?.secretKey)) {
 				throw new ApiError(meta.errors.unavailable);
 			}
 
-			const stripe = new Stripe(this.config.stripe.secretKey);
+			const plan = await this.subscriptionPlanRepository.findOneBy({ id: ps.planId });
+			if (plan?.isArchived || !plan?.stripePriceId) {
+				throw new ApiError(meta.errors.noSuchPlan);
+			}
 
 			let subscribeUser = await this.userProfilesRepository.findOneBy({ userId: me.id });
 			if (!subscribeUser) {
@@ -98,6 +97,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 				throw new ApiError(meta.errors.requiredEmail);
 			}
 
+			const stripe = new Stripe(this.config.stripe!.secretKey);
 			if (!subscribeUser.stripeCustomerId) {
 				const makeCustomer = await stripe.customers.create({
 					email: subscribeUser.email,
@@ -106,14 +106,6 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 					stripeCustomerId: makeCustomer.id,
 				});
 				subscribeUser = await this.userProfilesRepository.findOneByOrFail({ userId: me.id });
-			}
-
-			const role = await this.rolesRepository.findOneBy({ id: ps.roleId });
-			if (!role) {
-				throw new ApiError(meta.errors.noSuchRole);
-			}
-			if (!role.stripeProductId) {
-				throw new ApiError(meta.errors.noSuchSubscription);
 			}
 
 			const subscriptionStatus = subscribeUser.user!.subscriptionStatus; // null? wtf. really? why? how? when? where? who? what?
@@ -137,7 +129,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 					billing_address_collection: 'auto',
 					line_items: [
 						{
-							price: role.stripeProductId,
+							price: plan.stripePriceId,
 							quantity: 1,
 						},
 					],