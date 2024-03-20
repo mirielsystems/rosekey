ca6ef7d85ac93e5aae713f8fff1266387219fcba
ありす <master@16439s.jp>
2024-03-20 15:17:43 +0900

    Fix:

--- packages/frontend/test/home.test.ts
+++ packages/frontend/test/home.test.ts
@@ -1,8 +1,3 @@
-/*
- * SPDX-FileCopyrightText: syuilo and misskey-project
- * SPDX-License-Identifier: AGPL-3.0-only
- */
-
 import { afterEach, assert, describe, test } from 'vitest';
 import { cleanup, render, type RenderResult } from '@testing-library/vue';
 import './init';
@@ -13,56 +8,60 @@ import XHome from '@/pages/user/home.vue';
 import 'intersection-observer';
 
 describe('XHome', () => {
-	const renderHome = (user: Partial<Misskey.entities.UserDetailed>): RenderResult => {
-		return render(XHome, {
-			props: { user, disableNotes: true },
-			global: { directives, components },
-		});
-	};
+    const renderHome = (user: Partial<Misskey.entities.UserDetailed>): RenderResult => {
+        return render(XHome, {
+            props: { user, disableNotes: true },
+            global: { directives, components },
+        });
+    };
 
-	afterEach(() => {
-		cleanup();
-	});
+    afterEach(() => {
+        cleanup();
+    });
 
-	test('Should render the remote caution when user.host exists', async () => {
-		const home = renderHome({
-			id: 'blobcat',
-			name: 'blobcat',
-			host: 'example.com',
-			uri: 'https://example.com/@user',
-			url: 'https://example.com/@user/profile',
-			roles: [],
-			createdAt: '1970-01-01T00:00:00.000Z',
-			fields: [],
-			pinnedNotes: [],
-			avatarUrl: 'https://example.com',
-			avatarDecorations: [],
-		});
+    test('Should render the remote caution when user.host exists', async () => {
+        const home = renderHome({
+            id: 'blobcat',
+            name: 'blobcat',
+            host: 'example.com',
+            uri: 'https://example.com/@user',
+            url: 'https://example.com/@user/profile',
+            roles: [],
+            createdAt: '1970-01-01T00:00:00.000Z',
+            fields: [],
+            pinnedNotes: [],
+            avatarUrl: 'https://example.com',
+            avatarDecorations: [],
+        });
 
-		const anchor = home.container.querySelector<HTMLAnchorElement>('a[href^="https://example.com/"]');
-		assert.exists(anchor, 'anchor to the remote exists');
-		assert.strictEqual(anchor?.href, 'https://example.com/@user/profile');
+        const anchor = home.container.querySelector<HTMLAnchorElement>('a[href^="https://example.com/"]');
+        assert.exists(anchor, 'anchor to the remote exists');
+        assert.strictEqual(anchor?.href, 'https://example.com/@user/profile');
 
-		assert.ok(anchor?.parentElement?.classList.contains('warn'), 'the parent is a warning');
-	});
+        if (anchor?.parentElement) {
+            assert.ok(anchor.parentElement.classList.contains('warn'), 'the parent is a warning');
+        } else {
+            assert.fail('parent element is null');
+        }
+    });
 
-	test('The remote caution should fall back to uri if url is null', async () => {
-		const home = renderHome({
-			id: 'blobcat',
-			name: 'blobcat',
-			host: 'example.com',
-			uri: 'https://example.com/@user',
-			url: null,
-			roles: [],
-			createdAt: '1970-01-01T00:00:00.000Z',
-			fields: [],
-			pinnedNotes: [],
-			avatarUrl: 'https://example.com',
-			avatarDecorations: [],
-		});
+    test('The remote caution should fall back to uri if url is null', async () => {
+        const home = renderHome({
+            id: 'blobcat',
+            name: 'blobcat',
+            host: 'example.com',
+            uri: 'https://example.com/@user',
+            url: null,
+            roles: [],
+            createdAt: '1970-01-01T00:00:00.000Z',
+            fields: [],
+            pinnedNotes: [],
+            avatarUrl: 'https://example.com',
+            avatarDecorations: [],
+        });
 
-		const anchor = home.container.querySelector<HTMLAnchorElement>('a[href^="https://example.com/"]');
-		assert.exists(anchor, 'anchor to the remote exists');
-		assert.strictEqual(anchor?.href, 'https://example.com/@user');
-	});
+        const anchor = home.container.querySelector<HTMLAnchorElement>('a[href^="https://example.com/"]');
+        assert.exists(anchor, 'anchor to the remote exists');
+        assert.strictEqual(anchor?.href, 'https://example.com/@user');
+    });
 });
