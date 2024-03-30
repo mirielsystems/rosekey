157a188ae9ab0fe38bfdbe068ebb8ce9cc9c294a
master <master@164.one>
2024-03-30 18:11:31 +0900

    Fix: Typecheck P1

--- packages/backend/src/server/api/endpoints/admin/abuse-report-resolver/create.ts
+++ packages/backend/src/server/api/endpoints/admin/abuse-report-resolver/create.ts
@@ -12,59 +12,62 @@ import type { AbuseReportResolversRepository } from '@/models/_.js';
 import { IdService } from '@/core/IdService.js';
 
 export const meta = {
-	tags: ['admin'],
-
-	requireCredential: true,
-
-	requireAdmin: true,
-
-	res: {
-		type: 'object',
-		properties: {
-			name: {
-				type: 'string',
-				nullable: false, optional: false,
-			},
-			targetUserPattern: {
-				type: 'string',
-				nullable: true, optional: false,
-			},
-			reporterPattern: {
-				type: 'string',
-				nullable: true, optional: false,
-			},
-			reportContentPattern: {
-				type: 'string',
-				nullable: true, optional: false,
-			},
-			expiresAt: {
-				type: 'string',
-				nullable: false, optional: false,
-			},
-			forward: {
-				type: 'boolean',
-				nullable: false, optional: false,
-			},
-		},
-	},
-
-	errors: {
-		invalidRegularExpressionForTargetUser: {
-			message: 'Invalid regular expression for target user.',
-			code: 'INVALID_REGULAR_EXPRESSION_FOR_TARGET_USER',
-			id: 'c008484a-0a14-4e74-86f4-b176dc49fcaa',
-		},
-		invalidRegularExpressionForReporter: {
-			message: 'Invalid regular expression for reporter.',
-			code: 'INVALID_REGULAR_EXPRESSION_FOR_REPORTER',
-			id: '399b4062-257f-44c8-87cc-4ffae2527fbc',
-		},
-		invalidRegularExpressionForReportContent: {
-			message: 'Invalid regular expression for report content.',
-			code: 'INVALID_REGULAR_EXPRESSION_FOR_REPORT_CONTENT',
-			id: '88c124d8-f517-4c63-a464-0abc274168b',
-		},
-	},
+    tags: ['admin'],
+    requireCredential: true,
+    requireAdmin: true,
+    kind: 'abuse-report-resolver-create', // 任意の値を設定する
+    res: {
+        type: 'object',
+        properties: {
+            name: {
+                type: 'string',
+                nullable: false,
+                optional: false,
+            },
+            targetUserPattern: {
+                type: 'string',
+                nullable: true,
+                optional: false,
+            },
+            reporterPattern: {
+                type: 'string',
+                nullable: true,
+                optional: false,
+            },
+            reportContentPattern: {
+                type: 'string',
+                nullable: true,
+                optional: false,
+            },
+            expiresAt: {
+                type: 'string',
+                nullable: false,
+                optional: false,
+            },
+            forward: {
+                type: 'boolean',
+                nullable: false,
+                optional: false,
+            },
+        },
+    },
+    errors: {
+        invalidRegularExpressionForTargetUser: {
+            message: 'Invalid regular expression for target user.',
+            code: 'INVALID_REGULAR_EXPRESSION_FOR_TARGET_USER',
+            id: 'c008484a-0a14-4e74-86f4-b176dc49fcaa',
+        },
+        invalidRegularExpressionForReporter: {
+            message: 'Invalid regular expression for reporter.',
+            code: 'INVALID_REGULAR_EXPRESSION_FOR_REPORTER',
+            id: '399b4062-257f-44c8-87cc-4ffae2527fbc',
+        },
+        invalidRegularExpressionForReportContent: {
+            message: 'Invalid regular expression for report content.',
+            code: 'INVALID_REGULAR_EXPRESSION_FOR_REPORT_CONTENT',
+            id: '88c124d8-f517-4c63-a464-0abc274168b',
+        },
+    },
 } as const;
 
 export const paramDef = {
