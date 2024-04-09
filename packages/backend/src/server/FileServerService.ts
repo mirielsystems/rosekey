92eec2178fd103e9ea2bcd646aacab1fb496a33b
dakkar <dakkar@thenautilus.net>
2024-04-09 15:42:29 +0100

    return 206 for every ranged response - fixes #494

--- packages/backend/src/server/FileServerService.ts
+++ packages/backend/src/server/FileServerService.ts
@@ -192,6 +192,7 @@ export class FileServerService {
 						reply.header('Content-Range', `bytes ${start}-${end}/${file.file.size}`);
 						reply.header('Accept-Ranges', 'bytes');
 						reply.header('Content-Length', chunksize);
+						reply.code(206);
 					} else {
 						image = {
 							data: fs.createReadStream(file.path),
@@ -431,6 +432,7 @@ export class FileServerService {
 					reply.header('Content-Range', `bytes ${start}-${end}/${file.file.size}`);
 					reply.header('Accept-Ranges', 'bytes');
 					reply.header('Content-Length', chunksize);
+					reply.code(206);
 				} else {
 					image = {
 						data: fs.createReadStream(file.path),
