2aef9fb0b5f85f745df9fee7c93589166465806d
16439s <master@16439s.dev>
2024-04-12 23:15:02 +0900

    Fix: Oauth2

--- packages/backend/src/server/oauth/OAuth2ProviderService.ts
+++ packages/backend/src/server/oauth/OAuth2ProviderService.ts
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-FileCopyrightText: syuilo and other misskey contributors
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
@@ -10,45 +10,11 @@ import { v4 as uuid } from 'uuid';
 /* import { kinds } from '@/misc/api-permissions.js';
 import type { Config } from '@/config.js';
 import { DI } from '@/di-symbols.js'; */
+import multer from 'fastify-multer';
 import { bindThis } from '@/decorators.js';
 import type { Config } from '@/config.js';
 import { DI } from '@/di-symbols.js';
 import type { FastifyInstance } from 'fastify';
-
-const kinds = [
-	'read:account',
-	'write:account',
-	'read:blocks',
-	'write:blocks',
-	'read:drive',
-	'write:drive',
-	'read:favorites',
-	'write:favorites',
-	'read:following',
-	'write:following',
-	'read:messaging',
-	'write:messaging',
-	'read:mutes',
-	'write:mutes',
-	'write:notes',
-	'read:notifications',
-	'write:notifications',
-	'read:reactions',
-	'write:reactions',
-	'write:votes',
-	'read:pages',
-	'write:pages',
-	'write:page-likes',
-	'read:page-likes',
-	'read:user-groups',
-	'write:user-groups',
-	'read:channels',
-	'write:channels',
-	'read:gallery',
-	'write:gallery',
-	'read:gallery-likes',
-	'write:gallery-likes',
-];
 
 function getClient(BASE_URL: string, authorization: string | undefined): MegalodonInterface {
 	const accessTokenArr = authorization?.split(' ') ?? [null];
@@ -64,22 +30,6 @@ export class OAuth2ProviderService {
 		@Inject(DI.config)
 		private config: Config,
 	) { }
-
-	// https://datatracker.ietf.org/doc/html/rfc8414.html
-	// https://indieauth.spec.indieweb.org/#indieauth-server-metadata
-	public generateRFC8414() {
-		return {
-			issuer: this.config.url,
-			authorization_endpoint: new URL('/oauth/authorize', this.config.url),
-			token_endpoint: new URL('/oauth/token', this.config.url),
-			scopes_supported: kinds,
-			response_types_supported: ['code'],
-			grant_types_supported: ['authorization_code'],
-			service_documentation: 'https://misskey-hub.net',
-			code_challenge_methods_supported: ['S256'],
-			authorization_response_iss_parameter_supported: true,
-		};
-	}
 
 	@bindThis
 	public async createServer(fastify: FastifyInstance): Promise<void> {
@@ -98,6 +48,14 @@ export class OAuth2ProviderService {
 				authorization_response_iss_parameter_supported: true,
 			});
 		}); */
+
+		const upload = multer({
+			storage: multer.diskStorage({}),
+			limits: {
+				fileSize: this.config.maxFileSize || 262144000,
+				files: 1,
+			},
+		});
 
 		fastify.addHook('onRequest', (request, reply, done) => {
 			reply.header('Access-Control-Allow-Origin', '*');
@@ -120,32 +78,37 @@ export class OAuth2ProviderService {
 			payload.on('error', done);
 		});
 
+		fastify.register(multer.contentParser);
+
 		fastify.get('/oauth/authorize', async (request, reply) => {
 			const query: any = request.query;
-			let param = 'mastodon=true';
+			let param = "mastodon=true";
 			if (query.state) param += `&state=${query.state}`;
 			if (query.redirect_uri) param += `&redirect_uri=${query.redirect_uri}`;
-			const client = query.client_id ? query.client_id : '';
+			const client = query.client_id ? query.client_id : "";
 			reply.redirect(
 				`${Buffer.from(client.toString(), 'base64').toString()}?${param}`,
 			);
 		});
 
-		fastify.post('/oauth/decision', async (request, reply) => {
-			const body: any = request.body;
-			if (body.cancel) {
-				reply.send({ user: body.login_token, allow: false });
-			}
-			reply.send({ user: body.login_token, allow: true });
+		fastify.get('/oauth/authorize/', async (request, reply) => {
+			const query: any = request.query;
+			let param = "mastodon=true";
+			if (query.state) param += `&state=${query.state}`;
+			if (query.redirect_uri) param += `&redirect_uri=${query.redirect_uri}`;
+			const client = query.client_id ? query.client_id : "";
+			reply.redirect(
+				`${Buffer.from(client.toString(), 'base64').toString()}?${param}`,
+			);
 		});
 
-		fastify.post('/oauth/token', async (request, reply) => {
+		fastify.post('/oauth/token', { preHandler: upload.none() }, async (request, reply) => {
 			const body: any = request.body || request.query;
-			if (body.grant_type === 'client_credentials') {
+			if (body.grant_type === "client_credentials") {
 				const ret = {
 					access_token: uuid(),
-					token_type: 'Bearer',
-					scope: 'read',
+					token_type: "Bearer",
+					scope: "read",
 					created_at: Math.floor(new Date().getTime() / 1000),
 				};
 				reply.send(ret);
@@ -173,12 +136,12 @@ export class OAuth2ProviderService {
 				const atData = await client.fetchAccessToken(
 					client_id,
 					body.client_secret,
-					token ? token : '',
+					token ? token : "",
 				);
 				const ret = {
 					access_token: atData.accessToken,
-					token_type: 'Bearer',
-					scope: body.scope || 'read write follow push',
+					token_type: "Bearer",
+					scope: body.scope || "read write follow push",
 					created_at: Math.floor(new Date().getTime() / 1000),
 				};
 				reply.send(ret);
