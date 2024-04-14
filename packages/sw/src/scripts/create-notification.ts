transfem-org/Sharkey

add: Megalodon, initial mastodon api

packages/sw/src/scripts/create-notification.ts
@@ -250,7 +250,7 @@ export async function createEmptyNotification(): Promise<void> {
 		await globalThis.registration.showNotification(
 			(new URL(origin)).host,
 			{
-				body: `Misskey v${_VERSION_}`,
+				body: `Sharkey v${_VERSION_}`,
 				silent: true,
 				badge: iconUrl('null'),
 				tag: 'read_notification',

mirielsystems/rosekey

packages/sw/src/scripts/create-notification.ts
@@ -294,7 +294,7 @@ export async function createEmptyNotification(): Promise<void> {
 		await globalThis.registration.showNotification(
 			(new URL(origin)).host,
 			{
-				body: `CherryPick v${_VERSION_}`,
+				body: `Rosekey v${_VERSION_}`,
 				silent: true,
 				badge: iconUrl('null'),
 				tag: 'read_notification',