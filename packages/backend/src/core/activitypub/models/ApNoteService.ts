kokonect-link/cherrypick

Revert "drop messaging (#9919)"
This reverts commit 8f2049bc

packages/backend/src/core/activitypub/models/ApNoteService.ts
@@ -1,7 +1,7 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import promiseLimit from 'promise-limit';
 import { DI } from '@/di-symbols.js';
-import type { PollsRepository, EmojisRepository, UsersRepository } from '@/models/index.js';
+import type { MessagingMessagesRepository, PollsRepository, EmojisRepository, UsersRepository } from '@/models/index.js';
 import type { Config } from '@/config.js';
 import type { RemoteUser } from '@/models/entities/User.js';
 import type { Note } from '@/models/entities/Note.js';
@@ -16,6 +16,7 @@ import { IdService } from '@/core/IdService.js';
 import { PollService } from '@/core/PollService.js';
 import { StatusError } from '@/misc/status-error.js';
 import { UtilityService } from '@/core/UtilityService.js';
+import { MessagingService } from '@/core/MessagingService.js';
 import { bindThis } from '@/decorators.js';
 import { getOneApId, getApId, getOneApHrefNullable, validPost, isEmoji, getApType } from '../type.js';
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
@@ -46,6 +47,9 @@ export class ApNoteService {
 		@Inject(DI.emojisRepository)
 		private emojisRepository: EmojisRepository,
 
+		@Inject(DI.messagingMessagesRepository)
+		private messagingMessagesRepository: MessagingMessagesRepository,
+
 		private idService: IdService,
 		private apMfmService: ApMfmService,
 		private apResolverService: ApResolverService,
@@ -60,6 +64,7 @@ export class ApNoteService {
 		private apImageService: ApImageService,
 		private apQuestionService: ApQuestionService,
 		private metaService: MetaService,
+		private messagingService: MessagingService,
 		private appLockService: AppLockService,
 		private pollService: PollService,
 		private noteCreateService: NoteCreateService,
@@ -160,6 +165,8 @@ export class ApNoteService {
 			}
 		}
 	
+		let isMessaging = note._misskey_talk && visibility === 'specified';
+	
 		const apMentions = await this.apMentionService.extractApMentions(note.tag, resolver);
 		const apHashtags = await extractApHashtags(note.tag);
 	
@@ -186,6 +193,17 @@ export class ApNoteService {
 					return x;
 				}
 			}).catch(async err => {
+				// トークだったらinReplyToのエラーは無視
+				const uri = getApId(note.inReplyTo);
+				if (uri.startsWith(this.config.url + '/')) {
+					const id = uri.split('/').pop();
+					const talk = await this.messagingMessagesRepository.findOneBy({ id });
+					if (talk) {
+						isMessaging = true;
+						return null;
+					}
+				}
+	
 				this.logger.warn(`Error in inReplyTo ${note.inReplyTo} - ${err.statusCode ?? err}`);
 				throw err;
 			})
@@ -274,7 +292,14 @@ export class ApNoteService {
 		const apEmojis = emojis.map(emoji => emoji.name);
 	
 		const poll = await this.apQuestionService.extractPollFromQuestion(note, resolver).catch(() => undefined);
-		
+	
+		if (isMessaging) {
+			for (const recipient of visibleUsers) {
+				await this.messagingService.createMessage(actor, recipient, undefined, text ?? undefined, (files && files.length > 0) ? files[0] : null, object.id);
+				return null;
+			}
+		}
+	
 		return await this.noteCreateService.create(actor, {
 			createdAt: note.published ? new Date(note.published) : null,
 			files,

packages/backend/src/server/api/stream/channels/channel.ts
@@ -1,25 +1,32 @@
 import { Inject, Injectable } from '@nestjs/common';
+import type { NotesRepository, UsersRepository } from '@/models/index.js';
 import { isUserRelated } from '@/misc/is-user-related.js';
+import type { User } from '@/models/entities/User.js';
 import type { Packed } from '@/misc/schema.js';
 import { NoteEntityService } from '@/core/entities/NoteEntityService.js';
 import { UserEntityService } from '@/core/entities/UserEntityService.js';
 import { bindThis } from '@/decorators.js';
 import Channel from '../channel.js';
+import type { StreamMessages } from '../types.js';
 
 class ChannelChannel extends Channel {
 	public readonly chName = 'channel';
 	public static shouldShare = false;
 	public static requireCredential = false;
 	private channelId: string;
+	private typers: Record<User['id'], Date> = {};
+	private emitTypersIntervalId: ReturnType<typeof setInterval>;
 
 	constructor(
 		private noteEntityService: NoteEntityService,
+		private userEntityService: UserEntityService,
 
 		id: string,
 		connection: Channel['connection'],
 	) {
 		super(id, connection);
 		//this.onNote = this.onNote.bind(this);
+		//this.emitTypers = this.emitTypers.bind(this);
 	}
 
 	@bindThis
@@ -28,6 +35,8 @@ class ChannelChannel extends Channel {
 
 		// Subscribe stream
 		this.subscriber.on('notesStream', this.onNote);
+		this.subscriber.on(`channelStream:${this.channelId}`, this.onEvent);
+		this.emitTypersIntervalId = setInterval(this.emitTypers, 5000);
 	}
 
 	@bindThis
@@ -58,9 +67,41 @@ class ChannelChannel extends Channel {
 	}
 
 	@bindThis
+	private onEvent(data: StreamMessages['channel']['payload']) {
+		if (data.type === 'typing') {
+			const id = data.body;
+			const begin = this.typers[id] == null;
+			this.typers[id] = new Date();
+			if (begin) {
+				this.emitTypers();
+			}
+		}
+	}
+
+	@bindThis
+	private async emitTypers() {
+		const now = new Date();
+
+		// Remove not typing users
+		for (const [userId, date] of Object.entries(this.typers)) {
+			if (now.getTime() - date.getTime() > 5000) delete this.typers[userId];
+		}
+
+		const users = await this.userEntityService.packMany(Object.keys(this.typers), null, { detail: false });
+
+		this.send({
+			type: 'typers',
+			body: users,
+		});
+	}
+
+	@bindThis
 	public dispose() {
 		// Unsubscribe events
 		this.subscriber.off('notesStream', this.onNote);
+		this.subscriber.off(`channelStream:${this.channelId}`, this.onEvent);
+
+		clearInterval(this.emitTypersIntervalId);
 	}
 }
 
@@ -71,6 +112,7 @@ export class ChannelChannelService {
 
 	constructor(
 		private noteEntityService: NoteEntityService,
+		private userEntityService: UserEntityService,
 	) {
 	}
 
@@ -78,6 +120,7 @@ export class ChannelChannelService {
 	public create(id: string, connection: Channel['connection']): ChannelChannel {
 		return new ChannelChannel(
 			this.noteEntityService,
+			this.userEntityService,
 			id,
 			connection,
 		);

mirielsystems/rosekey

packages/backend/src/core/activitypub/models/ApNoteService.ts
@@ -7,7 +7,7 @@ import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import promiseLimit from 'promise-limit';
 import { In } from 'typeorm';
 import { DI } from '@/di-symbols.js';
-import type { PollsRepository, EmojisRepository, NotesRepository } from '@/models/_.js';
+import type { MessagingMessagesRepository, PollsRepository, EmojisRepository, NotesRepository } from '@/models/_.js';
 import type { Config } from '@/config.js';
 import type { MiRemoteUser } from '@/models/User.js';
 import type { MiNote } from '@/models/Note.js';
@@ -23,6 +23,7 @@ import { IdService } from '@/core/IdService.js';
 import { PollService } from '@/core/PollService.js';
 import { StatusError } from '@/misc/status-error.js';
 import { UtilityService } from '@/core/UtilityService.js';
+import { MessagingService } from '@/core/MessagingService.js';
 import { bindThis } from '@/decorators.js';
 import { checkHttps } from '@/misc/check-https.js';
 import { IdentifiableError } from '@/misc/identifiable-error.js';
@@ -58,6 +59,9 @@ export class ApNoteService {
 		@Inject(DI.emojisRepository)
 		private emojisRepository: EmojisRepository,
 
+		@Inject(DI.messagingMessagesRepository)
+		private messagingMessagesRepository: MessagingMessagesRepository,
+
 		private idService: IdService,
 		private apMfmService: ApMfmService,
 		private apResolverService: ApResolverService,
@@ -72,6 +76,7 @@ export class ApNoteService {
 		private apImageService: ApImageService,
 		private apQuestionService: ApQuestionService,
 		private metaService: MetaService,
+		private messagingService: MessagingService,
 		private appLockService: AppLockService,
 		private pollService: PollService,
 		private noteCreateService: NoteCreateService,
@@ -204,6 +209,7 @@ export class ApNoteService {
 		const noteAudience = await this.apAudienceService.parseAudience(actor, note.to, note.cc, resolver);
 		let visibility = noteAudience.visibility;
 		const visibleUsers = noteAudience.visibleUsers;
+		let isMessaging = note._misskey_talk && visibility === 'specified';
 
 		// Audience (to, cc) が指定されてなかった場合
 		if (visibility === 'specified' && visibleUsers.length === 0) {
@@ -236,6 +242,16 @@ export class ApNoteService {
 					return x;
 				})
 				.catch(async err => {
+					// トークだったらinReplyToのエラーは無視
+					const uri = getApId(note.inReplyTo);
+					if (uri.startsWith(this.config.url + '/')) {
+						const id = uri.split('/').pop();
+						const talk = await this.messagingMessagesRepository.findOneBy({ id });
+						if (talk) {
+							isMessaging = true;
+							return null;
+						}
+					}
 					this.logger.warn(`Error in inReplyTo ${note.inReplyTo} - ${err.statusCode ?? err}`);
 					throw err;
 				})
@@ -300,7 +316,14 @@ export class ApNoteService {
 		});
 
 		const apEmojis = emojis.map(emoji => emoji.name);
-
+	
+		if (isMessaging) {
+			for (const recipient of visibleUsers) {
+				await this.messagingService.createMessage(actor, recipient, undefined, text ?? undefined, (files && files.length > 0) ? files[0] : null, object.id);
+				return null;
+			}
+		}
+	
 		try {
 			return await this.noteCreateService.create(actor, {
 				createdAt: note.published ? new Date(note.published) : null,

packages/backend/src/server/api/stream/channels/channel.ts
@@ -6,24 +6,32 @@
 import { Injectable } from '@nestjs/common';
 import { isUserRelated } from '@/misc/is-user-related.js';
 import type { Packed } from '@/misc/json-schema.js';
+import type { NotesRepository, UsersRepository } from '@/models/_.js';
+import type { MiUser } from '@/models/User.js';
+import { UserEntityService } from '@/core/entities/UserEntityService.js';
 import { NoteEntityService } from '@/core/entities/NoteEntityService.js';
 import { bindThis } from '@/decorators.js';
 import Channel, { type MiChannelService } from '../channel.js';
+import type { StreamMessages } from '../types.js';
 
 class ChannelChannel extends Channel {
 	public readonly chName = 'channel';
 	public static shouldShare = false;
 	public static requireCredential = false as const;
 	private channelId: string;
+	private typers: Record<User['id'], Date> = {};
+	private emitTypersIntervalId: ReturnType<typeof setInterval>;
 
 	constructor(
 		private noteEntityService: NoteEntityService,
+		private userEntityService: UserEntityService,
 
 		id: string,
 		connection: Channel['connection'],
 	) {
 		super(id, connection);
 		//this.onNote = this.onNote.bind(this);
+		//this.emitTypers = this.emitTypers.bind(this);
 	}
 
 	@bindThis
@@ -32,6 +40,8 @@ class ChannelChannel extends Channel {
 
 		// Subscribe stream
 		this.subscriber.on('notesStream', this.onNote);
+		this.subscriber.on(`channelStream:${this.channelId}`, this.onEvent);
+		this.emitTypersIntervalId = setInterval(this.emitTypers, 5000);
 	}
 
 	@bindThis
@@ -58,9 +68,41 @@ class ChannelChannel extends Channel {
 	}
 
 	@bindThis
+	private onEvent(data: StreamMessages['channel']['payload']) {
+		if (data.type === 'typing') {
+			const id = data.body;
+			const begin = this.typers[id] == null;
+			this.typers[id] = new Date();
+			if (begin) {
+				this.emitTypers();
+			}
+		}
+	}
+
+	@bindThis
+	private async emitTypers() {
+		const now = new Date();
+
+		// Remove not typing users
+		for (const [userId, date] of Object.entries(this.typers)) {
+			if (now.getTime() - date.getTime() > 5000) delete this.typers[userId];
+		}
+
+		const users = await this.userEntityService.packMany(Object.keys(this.typers), null, { detail: false });
+
+		this.send({
+			type: 'typers',
+			body: users,
+		});
+	}
+
+	@bindThis
 	public dispose() {
 		// Unsubscribe events
 		this.subscriber.off('notesStream', this.onNote);
+		this.subscriber.off(`channelStream:${this.channelId}`, this.onEvent);
+
+		clearInterval(this.emitTypersIntervalId);
 	}
 }
 
@@ -72,6 +114,7 @@ export class ChannelChannelService implements MiChannelService<false> {
 
 	constructor(
 		private noteEntityService: NoteEntityService,
+		private userEntityService: UserEntityService,
 	) {
 	}
 
@@ -79,6 +122,7 @@ export class ChannelChannelService implements MiChannelService<false> {
 	public create(id: string, connection: Channel['connection']): ChannelChannel {
 		return new ChannelChannel(
 			this.noteEntityService,
+			this.userEntityService,
 			id,
 			connection,
 		);