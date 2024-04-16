38381a687e340e9839887a1461646df8834f7063
16439s <master@16439s.dev>
2024-04-16 22:25:46 +0900

    Fix?

--- packages/frontend/src/pages/messaging/index.vue
+++ packages/frontend/src/pages/messaging/index.vue
@@ -1,84 +1,74 @@
+<!--
+SPDX-FileCopyrightText: syuilo and misskey-project & noridev and cherrypick-project
+SPDX-License-Identifier: AGPL-3.0-only
+-->
+
 <template>
 <MkStickyContainer>
-	<template #header><MkPageHeader :actions="headerActions" :tabs="headerTabs"/></template>
+	<template #header><MkPageHeader v-model:tab="tab" :actions="headerActions" :tabs="headerTabs"/></template>
 	<MkSpacer :contentMax="800">
-		<div class="yweeujhr">
-			<MkButton primary class="start" @click="start"><i class="ti ti-plus"></i> {{ $ts.startMessaging }}</MkButton>
-
-			<div v-if="messages.length > 0" class="history">
-				<MkA
-					v-for="(message, i) in messages"
-					:key="message.id"
-					v-anim="i"
-					class="message _panel"
-					:class="{ isMe: isMe(message), isRead: message.groupId ? message.reads.includes($i.id) : message.isRead }"
-					:to="message.groupId ? `/my/messaging/group/${message.groupId}` : `/my/messaging/${getAcct(isMe(message) ? message.recipient : message.user)}`"
-					:data-index="i"
-				>
-					<div>
-						<MkAvatar class="avatar" :user="message.groupId ? message.user : isMe(message) ? message.recipient : message.user" indicator link preview/>
-						<header v-if="message.groupId">
-							<span class="name">{{ message.group.name }}</span>
-							<MkTime :time="message.createdAt" class="time"/>
-						</header>
-						<header v-else>
-							<span class="name"><MkUserName :user="isMe(message) ? message.recipient : message.user"/></span>
-							<span class="username">@{{ acct(isMe(message) ? message.recipient : message.user) }}</span>
-							<MkTime :time="message.createdAt" class="time"/>
-						</header>
-						<div class="body">
-							<p class="text"><span v-if="isMe(message)" class="me">{{ $ts.you }}:</span>{{ message.text }}</p>
-						</div>
-					</div>
-				</MkA>
+		<div>
+			<div v-if="tab === 'direct'">
+				<MkPagination v-slot="{ items }" ref="pagingComponent" :pagination="directPagination">
+					<MkChatPreview v-for="message in items" :key="message.id" :message="message"/>
+				</MkPagination>
 			</div>
-			<div v-if="!fetching && messages.length == 0" class="_fullinfo">
-				<img src="https://xn--931a.moe/assets/info.jpg" class="_ghost"/>
-				<div>{{ $ts.noHistory }}</div>
-			</div>
-			<MkLoading v-if="fetching"/>
 		</div>
 	</MkSpacer>
 </MkStickyContainer>
 </template>
-
+	
 <script lang="ts" setup>
-import { ref, computed, defineAsyncComponent, defineComponent, inject, markRaw, onMounted, onUnmounted } from 'vue';
-import * as Misskey from 'misskey-js';
-import MkButton from '@/components/MkButton.vue';
-import { acct } from '@/filters/user';
-import * as os from '@/os';
-import { useStream } from '@/stream';
+import { computed, markRaw, onActivated, onMounted, onUnmounted, ref, shallowRef } from 'vue';
+import * as Misskey from 'cherrypick-js';
+import * as os from '@/os.js';
+import { misskeyApi } from '@/scripts/misskey-api.js';
+import { useStream } from '@/stream.js';
 import { useRouter } from '@/router/supplier.js';
-import { i18n } from '@/i18n';
-import { definePageMetadata } from '@/scripts/page-metadata';
-import { $i } from '@/account';
-
+import { i18n } from '@/i18n.js';
+import { definePageMetadata } from '@/scripts/page-metadata.js';
+import { $i } from '@/account.js';
+import { globalEvents } from '@/events.js';
+import MkChatPreview from '@/components/MkChatPreview.vue';
+import MkPagination from '@/components/MkPagination.vue';
+	
+const pagingComponent = shallowRef<InstanceType<typeof MkPagination>>();
+	
 const router = useRouter();
-
-const fetching = ref(true);
+	
+const tab = ref('direct');
+	
 let messages;
 let connection;
-
-const getAcct = acct.toString;
-
-function isMe(message) {
-	return message.userId === $i.id;
-}
-
+	
+const directPagination = {
+	endpoint: 'messaging/history' as const,
+	limit: 15,
+	params: {
+		group: false,
+	},
+};
+const groupsPagination = {
+	endpoint: 'messaging/history' as const,
+	limit: 5,
+	params: {
+		group: true,
+	},
+};
+	
 function onMessage(message) {
 	if (message.recipientId) {
 		messages = messages.filter(m => !(
 			(m.recipientId === message.recipientId && m.userId === message.userId) ||
-			(m.recipientId === message.userId && m.userId === message.recipientId)));
-
+				(m.recipientId === message.userId && m.userId === message.recipientId)));
+	
 		messages.unshift(message);
 	} else if (message.groupId) {
 		messages = messages.filter(m => m.groupId !== message.groupId);
 		messages.unshift(message);
 	}
 }
-
+	
 function onRead(ids) {
 	for (const id of ids) {
 		const found = messages.find(m => m.id === id);
@@ -91,214 +81,78 @@ function onRead(ids) {
 		}
 	}
 }
-
+	
 function start(ev) {
 	os.popupMenu([{
 		text: i18n.ts.messagingWithUser,
 		icon: 'ti ti-user',
 		action: () => { startUser(); },
-	}, {
-		text: i18n.ts.messagingWithGroup,
-		icon: 'ti ti-users',
-		action: () => { startGroup(); },
 	}], ev.currentTarget ?? ev.target);
 }
-
+	
 async function startUser() {
 	os.selectUser().then(user => {
-		router.push(`/my/messaging/${acct.toString()}`);
+		router.push(`/my/messaging/@${Misskey.acct.toString(user)}`);
 	});
 }
 
-async function startGroup() {
-	const groups1 = await os.api('users/groups/owned');
-	const groups2 = await os.api('users/groups/joined');
-	if (groups1.length === 0 && groups2.length === 0) {
-		os.alert({
-			type: 'warning',
-			title: i18n.ts.youHaveNoGroups,
-			text: i18n.ts.joinOrCreateGroup,
-		});
-		return;
-	}
-	const { canceled, result: group } = await os.select({
-		title: i18n.ts.group,
-		items: groups1.concat(groups2).map(group => ({
-			value: group, text: group.name,
-		})),
-	});
-	if (canceled) return;
-	router.push(`/my/messaging/group/${group.id}`);
+async function readAllMessagingMessages() {
+	await os.apiWithDialog('i/read-all-messaging-messages');
 }
-
+	
 onMounted(() => {
 	connection = markRaw(useStream().useChannel('messagingIndex'));
-
+	
 	connection.on('message', onMessage);
 	connection.on('read', onRead);
-
-	os.api('messaging/history', { group: false }).then(userMessages => {
-		os.api('messaging/history', { group: true }).then(groupMessages => {
+	
+	misskeyApi('messaging/history', { group: false }).then(userMessages => {
+		misskeyApi('messaging/history', { group: true }).then(groupMessages => {
 			const _messages = userMessages.concat(groupMessages);
 			_messages.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
 			messages = _messages;
-			fetching.value = false;
 		});
 	});
+	
+	globalEvents.on('openMessage', (ev) => {
+		start(ev);
+	});
 });
-
+	
+onActivated(() => {
+	pagingComponent.value?.reload();
+});
+	
 onUnmounted(() => {
 	if (connection) connection.dispose();
 });
-
-const headerActions = computed(() => []);
-
-const headerTabs = computed(() => []);
-
-definePageMetadata({
+	
+const headerActions = computed(() => [{
+	icon: 'ti ti-plus',
+	text: i18n.ts.create,
+	handler: start,
+}, {
+	icon: 'ti ti-check',
+	text: i18n.ts.markAllAsRead,
+	handler: readAllMessagingMessages,
+}]);
+	
+const headerTabs = computed(() => [{
+	key: 'direct',
+	title: i18n.ts._messaging.direct,
+	icon: 'ti ti-users',
+}, {
+	key: 'groups',
+	title: i18n.ts.groups,
+	icon: 'ti ti-users-group',
+}]);
+	
+definePageMetadata(() => ({
 	title: i18n.ts.messaging,
 	icon: 'ti ti-messages',
-});
+}));
 </script>
-
-<style lang="scss" scoped>
-.yweeujhr {
-
-	> .start {
-		margin: 0 auto var(--margin) auto;
-	}
-
-	> .history {
-		> .message {
-			display: block;
-			text-decoration: none;
-			margin-bottom: var(--margin);
-
-			* {
-				pointer-events: none;
-				user-select: none;
-			}
-
-			&:hover {
-				.avatar {
-					filter: saturate(200%);
-				}
-			}
-
-			&:active {
-			}
-
-			&.isRead,
-			&.isMe {
-				opacity: 0.8;
-			}
-
-			&:not(.isMe):not(.isRead) {
-				> div {
-					background-image: url("/client-assets/unread.svg");
-					background-repeat: no-repeat;
-					background-position: 0 center;
-				}
-			}
-
-			&:after {
-				content: "";
-				display: block;
-				clear: both;
-			}
-
-			> div {
-				padding: 20px 30px;
-
-				&:after {
-					content: "";
-					display: block;
-					clear: both;
-				}
-
-				> header {
-					display: flex;
-					align-items: center;
-					margin-bottom: 2px;
-					white-space: nowrap;
-					overflow: hidden;
-
-					> .name {
-						margin: 0;
-						padding: 0;
-						overflow: hidden;
-						text-overflow: ellipsis;
-						font-size: 1em;
-						font-weight: bold;
-						transition: all 0.1s ease;
-					}
-
-					> .username {
-						margin: 0 8px;
-					}
-
-					> .time {
-						margin: 0 0 0 auto;
-					}
-				}
-
-				> .avatar {
-					float: left;
-					width: 54px;
-					height: 54px;
-					margin: 0 16px 0 0;
-					border-radius: 8px;
-					transition: all 0.1s ease;
-				}
-
-				> .body {
-
-					> .text {
-						display: block;
-						margin: 0 0 0 0;
-						padding: 0;
-						overflow: hidden;
-						overflow-wrap: break-word;
-						font-size: 1.1em;
-						color: var(--faceText);
-
-						.me {
-							opacity: 0.7;
-						}
-					}
-
-					> .image {
-						display: block;
-						max-width: 100%;
-						max-height: 512px;
-					}
-				}
-			}
-		}
-	}
-}
-
-@container (max-width: 400px) {
-	.yweeujhr {
-		> .history {
-			> .message {
-				&:not(.isMe):not(.isRead) {
-					> div {
-						background-image: none;
-						border-left: solid 4px #3aa2dc;
-					}
-				}
-
-				> div {
-					padding: 16px;
-					font-size: 0.9em;
-
-					> .avatar {
-						margin: 0 12px 0 0;
-					}
-				}
-			}
-		}
-	}
-}
-</style>
+	
+	<style lang="scss" module>
+	</style>
+	
