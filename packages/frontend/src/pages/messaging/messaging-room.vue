07e93c2be903bb76f9df61c17608d335c12c9ffd
16439s <master@16439s.dev>
2024-04-17 16:03:07 +0900

    Update messaging-room.vue

--- packages/frontend/src/pages/messaging/messaging-room.vue
+++ packages/frontend/src/pages/messaging/messaging-room.vue
@@ -22,7 +22,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 							:class="{ [$style.messages]: true, 'deny-move-transition': pFetching }"
 							:items="messages"
 							direction="up"
-							reversed
 						>
 							<XMessage :key="message.id" :message="message" :isGroup="group != null"/>
 						</MkDateSeparatedList>
@@ -210,16 +209,16 @@ function onDrop(ev: DragEvent): void {
 function onMessage(message) {
 	sound.playMisskeySfx('chat');
 	vibrate(defaultStore.state.vibrateChat ? [30, 30, 30] : []);
-	
+
 	const _isBottom = isBottomVisible(rootEl.value, 64);
-	
-	pagingComponent.value.prepend(message);
+
+	pagingComponent.value.append(message); // append to add to the end
 	if (message.userId !== $i?.id && !document.hidden) {
 		connection.value?.send('read', {
 			id: message.id,
 		});
 	}
-	
+
 	if (_isBottom) {
 		// Scroll to bottom
 		nextTick(() => {
