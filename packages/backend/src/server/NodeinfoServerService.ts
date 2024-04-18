4586dc32f8e70f6066e5b094686e9d7bb4e47ec2
16439s <master@16439s.dev>
2024-04-18 16:47:03 +0900

    Feat: kmyblueの開示AP仕様に対応
    https://github.com/kmycode/mastodon/wiki/%E5%88%A9%E7%94%A8%E5%8F%AF%E8%83%BD%E6%A9%9F%E8%83%BD%E4%B8%80%E8%A6%A7%E9%96%8B%E7%A4%BAAP%E4%BB%95%E6%A7%98
    に対応

--- packages/backend/src/server/NodeinfoServerService.ts
+++ packages/backend/src/server/NodeinfoServerService.ts
@@ -125,6 +125,7 @@ export class NodeinfoServerService {
 					enableServiceWorker: meta.enableServiceWorker,
 					proxyAccountName: proxyAccount ? proxyAccount.username : null,
 					themeColor: meta.themeColor ?? '#86b300',
+					features: ['emoji_reaction', 'quote'],
 				},
 			};
 			if (version >= 21) {
