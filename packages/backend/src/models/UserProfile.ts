c9c3e27bb123a96fc65fc68f14dbcd5f31ff0915
16439s <master@16439s.dev>
2024-04-22 09:51:20 +0900

    Feat: 推し登録機能 for backend

--- packages/backend/src/models/UserProfile.ts
+++ packages/backend/src/models/UserProfile.ts
@@ -36,13 +36,27 @@ export class MiUserProfile {
 	})
 	public birthday: string | null;
 
-	@Column("varchar", {
+	@Column('varchar', {
 		length: 128,
 		nullable: true,
-		comment: "The ListenBrainz username of the User.",
+		comment: 'The ListenBrainz username of the User.',
 	})
 	public listenbrainz: string | null;
 
+	@Column('varchar', {
+		length: 128,
+		nullable: true,
+		comment: 'The Oshi of the User.',
+	})
+	public oshi: string | null;
+
+	@Index()
+	@Column('char', {
+		length: 10, nullable: true,
+		comment: 'The oshi start date (YYYY-MM-DD) of the User.',
+	})
+	public oshistartdate: string | null;
+	
 	@Column('varchar', {
 		length: 2048, nullable: true,
 		comment: 'The description (bio) of the User.',
