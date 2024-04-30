d09311d641f3244da8e76713811c54fbe4425df5
まっちゃとーにゅ <17376330+u1-liquid@users.noreply.github.com>
2024-04-30 14:32:30 +0900

    enhance(frontend): パスワード変更時にHIBPで流出パスワードをチェックするように (MisskeyIO#625)

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -9053,6 +9053,25 @@ export interface Locale extends ILocale {
          * 詳細なガイドはこちら
          */
         "moreDetailedGuideHere": string;
+        /**
+         * バックアップコードを保存しましたか？
+         */
+        "backupCodesSavedConfirmTitle": string;
+        /**
+         * 二要素認証アプリとバックアップコードの両方を紛失した場合、アカウントにアクセスできなくなります。
+         * 誰とも共有せず、適切な方法で保管してください。
+         *
+         * $[x2 二要素認証設定は自分以外の誰にも変更できませんので、$[fg.color=red 運営チームも無効化することはできません。]]
+         */
+        "backupCodesSavedConfirmDescription": string;
+        /**
+         * バックアップコードを保存しました
+         */
+        "backupCodesSavedConfirmChecked": string;
+        /**
+         * 設定方法でお困りの際は、{link}を参照してください。
+         */
+        "detailedGuide": ParameterizedString<"link">;
     };
     "_permissions": {
         /**
