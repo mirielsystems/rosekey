16033b131a5002afb1ae69d3afaf04b1d84521a1
miriel <me@miriel.net>
2024-05-01 20:45:02 +0900

    Fix

--- packages/backend/src/core/activitypub/misc/contexts.ts
+++ packages/backend/src/core/activitypub/misc/contexts.ts
@@ -554,9 +554,17 @@ const extension_context_definition = {
 	'_misskey_reaction': 'misskey:_misskey_reaction',
 	'_misskey_votes': 'misskey:_misskey_votes',
 	'_misskey_summary': 'misskey:_misskey_summary',
+	'_misskey_talk': 'misskey:_misskey_talk',
 	'isCat': 'misskey:isCat',
+	// Firefish
+	firefish: 'https://joinfirefish.org/ns#',
+	speakAsCat: 'firefish:speakAsCat',
 	// vcard
 	vcard: 'http://www.w3.org/2006/vcard/ns#',
+	// rosekey＆sharkey
+	listenbrainz: 'sharkey:listenbrainz',
+	oshi: 'rosekey:oshi',
+	oshistartdate: 'rosekey:oshistartdate',
 } satisfies Context;
 
 export const CONTEXT: (string | Context)[] = [...context_iris, extension_context_definition];
