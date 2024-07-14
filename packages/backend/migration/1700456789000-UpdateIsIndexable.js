a453f5ab51f7aafc132af501118e901c1e42c689
Miriel <me@miriel.net>
2024-07-14 12:27:24 +0900

    Fix: 07237d5f3f54d8ee8e1bdfac249a346479760120

--- packages/backend/migration/1700456789000-UpdateIsIndexable.js
+++ packages/backend/migration/1700456789000-UpdateIsIndexable.js
@@ -2,11 +2,15 @@ export class Updatenoindex1700456789000 {
     name = 'Updatenoindex1700456789000'
 
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "noindex" TO "noindex"`);
+        // Rename column isindexable to noindex
+        await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "isindexable" TO "noindex"`);
+        
+        // Set default value of noindex to false
         await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "noindex" SET DEFAULT false`);
-        await queryRunner.query(`UPDATE "user" SET "noindex" = NOT "noindex"`);
+        
+        // Optionally update all existing records if needed
+        await queryRunner.query(`UPDATE "user" SET "noindex" = NOT "noindex" WHERE "noindex" IS NULL`);
     }
-
 
 //    async down(queryRunner) {
 //        await queryRunner.query(`UPDATE "user" SET "noindex" = NOT "noindex"`);
