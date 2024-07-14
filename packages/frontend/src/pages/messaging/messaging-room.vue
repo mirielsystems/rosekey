cd31eed9e20da94484af5298da29e2a4294c2e4e
Miriel <me@miriel.net>
2024-07-14 17:45:51 +0900

    Fix

--- packages/frontend/src/pages/messaging/messaging-room.vue
+++ packages/frontend/src/pages/messaging/messaging-room.vue
@@ -55,7 +55,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 	</div>
 </MkStickyContainer>
 </template>
-
+	
 <script lang="ts" setup>
 import { computed, onMounted, nextTick, onBeforeUnmount, watch, shallowRef, ref } from 'vue';
 import * as Misskey from 'cherrypick-js';
@@ -74,18 +74,18 @@ import { definePageMetadata } from '@/scripts/page-metadata.js';
 import { vibrate } from '@/scripts/vibrate.js';
 import { miLocalStorage } from '@/local-storage.js';
 import { misskeyApi } from '@/scripts/misskey-api.js';
-
+	
 const isFriendly = ref(miLocalStorage.getItem('ui') === 'friendly');
-
+	
 const props = defineProps<{
-	userAcct?: string;
-	groupId?: string;
-}>();
-
+		userAcct?: string;
+		groupId?: string;
+	}>();
+	
 const rootEl = shallowRef<HTMLDivElement>();
 const formEl = shallowRef<InstanceType<typeof XForm>>();
 const pagingComponent = shallowRef<InstanceType<typeof MkPagination>>();
-
+	
 const fetching = ref(true);
 const user = ref<Misskey.entities.UserDetailed | null>(null);
 const group = ref<Misskey.entities.UserGroup | null>(null);
@@ -93,22 +93,23 @@ const typers = ref<Misskey.entities.User[]>([]);
 const connection = ref<Misskey.ChannelConnection<Misskey.Channels['messaging']> | null>(null);
 const showIndicator = ref(false);
 const animation = defaultStore.reactiveState;
-
+	
 const pagination = ref<Paging | null>(null);
-
+const scrollRemove = ref<(() => void) | null>(null);
+	
 watch([() => props.userAcct, () => props.groupId], () => {
 	if (connection.value) connection.value.dispose();
 	fetch();
 });
-
+	
 async function fetch() {
 	fetching.value = true;
-
+	
 	if (props.userAcct) {
 		const acct = Misskey.acct.parse(props.userAcct);
 		user.value = await misskeyApi('users/show', { username: acct.username, host: acct.host || undefined });
 		group.value = null;
-
+	
 		pagination.value = {
 			endpoint: 'messaging/messages',
 			limit: 20,
@@ -124,7 +125,7 @@ async function fetch() {
 	} else {
 		user.value = null;
 		group.value = await misskeyApi('users/groups/show', { groupId: props.groupId });
-
+	
 		pagination.value = {
 			endpoint: 'messaging/messages',
 			limit: 20,
@@ -138,16 +139,16 @@ async function fetch() {
 			group: group.value.id,
 		});
 	}
-
+	
 	connection.value.on('message', onMessage);
 	connection.value.on('read', onRead);
 	connection.value.on('deleted', onDeleted);
 	connection.value.on('typers', _typers => {
 		typers.value = _typers.filter(u => u.id !== $i?.id);
 	});
-
+	
 	document.addEventListener('visibilitychange', onVisibilitychange);
-
+	
 	nextTick(() => {
 		thisScrollToBottom();
 		window.setTimeout(() => {
@@ -155,13 +156,13 @@ async function fetch() {
 		}, 300);
 	});
 }
-
+	
 function onDragover(ev: DragEvent) {
 	if (!ev.dataTransfer) return;
-
+	
 	const isFile = ev.dataTransfer.items[0].kind === 'file';
 	const isDriveFile = ev.dataTransfer.types[0] === _DATA_TRANSFER_DRIVE_FILE_;
-
+	
 	if (isFile || isDriveFile) {
 		switch (ev.dataTransfer.effectAllowed) {
 			case 'all':
@@ -183,10 +184,10 @@ function onDragover(ev: DragEvent) {
 		ev.dataTransfer.dropEffect = 'none';
 	}
 }
-
+	
 function onDrop(ev: DragEvent): void {
 	if (!ev.dataTransfer) return;
-
+	
 	// ファイルだったら
 	if (ev.dataTransfer.files.length === 1) {
 		formEl.value.upload(ev.dataTransfer.files[0]);
@@ -198,7 +199,7 @@ function onDrop(ev: DragEvent): void {
 		});
 		return;
 	}
-
+	
 	//#region ドライブのファイル
 	const driveFile = ev.dataTransfer.getData(_DATA_TRANSFER_DRIVE_FILE_);
 	if (driveFile != null && driveFile !== '') {
@@ -206,7 +207,7 @@ function onDrop(ev: DragEvent): void {
 	}
 	//#endregion
 }
-
+	
 function onRead(x) {
 	if (user.value) {
 		if (!Array.isArray(x)) x = [x];
@@ -231,11 +232,32 @@ function onRead(x) {
 		}
 	}
 }
-
+	
 function onDeleted(id) {
 	pagingComponent.value.items.delete(id);
 }
-
+	
+function onMessage(message) {
+	sound.playMisskeySfx('chat');
+	vibrate(defaultStore.state.vibrateChat ? [30, 30, 30] : []);
+	
+	pagingComponent.value.prepend(message);
+	
+	// メッセージの順序を再度ソートする
+	pagingComponent.value.items.sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime());
+	
+	if (message.userId !== $i?.id && !document.hidden) {
+		connection.value?.send('read', {
+			id: message.id,
+		});
+	}
+	
+	// 常にボトムにスクロールする
+	nextTick(() => {
+		thisScrollToBottom();
+	});
+}
+	
 function onIndicatorClick() {
     // インジケーターを非表示にする
     showIndicator.value = false;
@@ -245,53 +267,26 @@ function onIndicatorClick() {
         thisScrollToBottom();
     });
 }
-
+	
 function thisScrollToBottom() {
-    if (window.location.href.includes('my/messaging/')) {
-        scrollToBottom(rootEl.value, { behavior: 'smooth' });
-    }
+	if (window.location.href.includes('my/messaging/')) {
+		scrollToBottom(rootEl.value, { behavior: 'auto' });
+	}
 }
-
-function onMessage(message) {
-    sound.playMisskeySfx('chat');
-    vibrate(defaultStore.state.vibrateChat ? [30, 30, 30] : []);
-
-    const _isBottom = isBottomVisible(rootEl.value, 64);
-
-    pagingComponent.value.prepend(message);
-
-    // メッセージの順序を再度ソートする
-    pagingComponent.value.items.sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime());
-
-    if (message.userId !== $i?.id && !document.hidden) {
-        connection.value?.send('read', {
-            id: message.id,
-        });
-    }
-
-    if (_isBottom) {
-        nextTick(() => {
-            thisScrollToBottom();
-        });
-    } else if (message.userId !== $i?.id) {
-        notifyNewMessage();
-    }
-}
-
+	
 function notifyNewMessage() {
-    showIndicator.value = true;
-
-    if (scrollRemove.value) {
-        scrollRemove.value();
-    }
-
-    scrollRemove.value = onScrollBottom(rootEl.value, () => {
-        showIndicator.value = false;
-        scrollRemove.value = null;
-    });
+	showIndicator.value = true;
+	
+	if (scrollRemove.value) {
+		scrollRemove.value();
+	}
+	
+	scrollRemove.value = onScrollBottom(rootEl.value, () => {
+		showIndicator.value = false;
+		scrollRemove.value = null;
+	});
 }
-
-
+	
 function onVisibilitychange() {
 	if (document.hidden) return;
 	for (const message of pagingComponent.value.items) {
@@ -302,17 +297,17 @@ function onVisibilitychange() {
 		}
 	}
 }
-
+	
 onMounted(() => {
 	fetch();
 });
-
+	
 onBeforeUnmount(() => {
 	connection.value?.dispose();
 	document.removeEventListener('visibilitychange', onVisibilitychange);
 	if (scrollRemove.value) scrollRemove();
 });
-
+	
 definePageMetadata(computed(() => !fetching.value ? user.value ? {
 	title: '',
 	icon: null,
@@ -323,125 +318,126 @@ definePageMetadata(computed(() => !fetching.value ? user.value ? {
 	icon: 'ti ti-users',
 } : null));
 </script>
-
-<style lang="scss" module>
-.fade-enter-active, .fade-leave-active {
-	transition: opacity 0.1s;
-}
-
-.fade-enter-from, .fade-leave-to {
-	transition: opacity 0.5s;
-	opacity: 0;
-}
-
-.body {
-	min-height: 80dvh;
-}
-
-.more {
-	display: block;
-	margin: 16px auto;
-	padding: 0 12px;
-	line-height: 24px;
-	color: #fff;
-	background: rgba(#000, 0.3);
-	border-radius: 12px;
-
-	&:hover {
-		background: rgba(#000, 0.4);
+	
+	<style lang="scss" module>
+	.fade-enter-active, .fade-leave-active {
+		transition: opacity 0.1s;
 	}
-
-	&:active {
-		background: rgba(#000, 0.5);
+	
+	.fade-enter-from, .fade-leave-to {
+		transition: opacity 0.5s;
+		opacity: 0;
 	}
-
-	> i {
-		margin-right: 4px;
+	
+	.body {
+		min-height: 80dvh;
 	}
-}
-
-.fetching {
-	cursor: wait;
-}
-
-.messages {
-	padding: 16px 0 0;
-
-	> * {
-		margin-bottom: 16px;
+	
+	.more {
+		display: block;
+		margin: 16px auto;
+		padding: 0 12px;
+		line-height: 24px;
+		color: #fff;
+		background: rgba(#000, 0.3);
+		border-radius: 12px;
+	
+		&:hover {
+			background: rgba(#000, 0.4);
+		}
+	
+		&:active {
+			background: rgba(#000, 0.5);
+		}
+	
+		> i {
+			margin-right: 4px;
+		}
 	}
-}
-
-.footerSpacer {
-  padding: 20px;
-}
-
-.footer {
-	width: 100%;
-	position: sticky;
-	z-index: 2;
-	padding-top: 8px;
-}
-
-.newMessage {
-	width: 100%;
-	padding-bottom: 8px;
-	text-align: center;
-}
-
-.newMessageButton {
-	display: inline-block;
-	margin: 0;
-	padding: 0 12px;
-	line-height: 32px;
-	font-size: 12px;
-	border-radius: 16px;
-}
-
-.newMessageIcon {
-	display: inline-block;
-	margin-right: 8px;
-}
-
-.typers {
-	position: absolute;
-	bottom: 100%;
-	padding: 0 8px 0 8px;
-	font-size: 0.9em;
-	color: var(--fgTransparentWeak);
-}
-
-.user + .user:before {
-	content: ", ";
-	font-weight: normal;
-}
-
-.user:last-of-type:after {
-	content: " ";
-}
-
-.form {
-	max-height: 12em;
-	overflow-y: scroll;
-	// border-top: solid 0.5px var(--divider);
-	// border-bottom-left-radius: 0;
-	// border-bottom-right-radius: 0;
-  border-radius: 15px;
-}
-
-@container (max-width: 500px) {
-  .footerSpacer {
-    padding: initial;
-  }
-
+	
+	.fetching {
+		cursor: wait;
+	}
+	
+	.messages {
+		padding: 16px 0 0;
+	
+		> * {
+			margin-bottom: 16px;
+		}
+	}
+	
+	.footerSpacer {
+	  padding: 20px;
+	}
+	
 	.footer {
-    &.friendly {
-      margin-bottom: calc(50px + env(safe-area-inset-bottom));
-    }
+		width: 100%;
+		position: sticky;
+		z-index: 2;
+		padding-top: 8px;
 	}
-
-  .form {
-    border-radius: 0;
-  }
-}
-</style>
+	
+	.newMessage {
+		width: 100%;
+		padding-bottom: 8px;
+		text-align: center;
+	}
+	
+	.newMessageButton {
+		display: inline-block;
+		margin: 0;
+		padding: 0 12px;
+		line-height: 32px;
+		font-size: 12px;
+		border-radius: 16px;
+	}
+	
+	.newMessageIcon {
+		display: inline-block;
+		margin-right: 8px;
+	}
+	
+	.typers {
+		position: absolute;
+		bottom: 100%;
+		padding: 0 8px 0 8px;
+		font-size: 0.9em;
+		color: var(--fgTransparentWeak);
+	}
+	
+	.user + .user:before {
+		content: ", ";
+		font-weight: normal;
+	}
+	
+	.user:last-of-type:after {
+		content: " ";
+	}
+	
+	.form {
+		max-height: 12em;
+		overflow-y: scroll;
+		// border-top: solid 0.5px var(--divider);
+		// border-bottom-left-radius: 0;
+		// border-bottom-right-radius: 0;
+	  border-radius: 15px;
+	}
+	
+	@container (max-width: 500px) {
+	  .footerSpacer {
+		padding: initial;
+	  }
+	
+		.footer {
+		&.friendly {
+		  margin-bottom: calc(50px + env(safe-area-inset-bottom));
+		}
+		}
+	
+	  .form {
+		border-radius: 0;
+	  }
+	}
+	</style>
+	
