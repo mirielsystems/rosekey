533b186ebe4ac277917401b36b5d1e0ed55bfe16
Miriel <me@miriel.net>
2024-07-14 12:31:11 +0900

    Chore

--- packages/backend/migration/1700456789000-UpdateIsIndexable.js
+++ packages/backend/migration/1700456789000-UpdateIsIndexable.js
@@ -3,7 +3,7 @@ export class Updatenoindex1700456789000 {
 
     async up(queryRunner) {
         // Rename column isindexable to noindex
-        await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "isindexable" TO "noindex"`);
+        await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "isIndexable" TO "noindex"`);
         
         // Set default value of noindex to false
         await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "noindex" SET DEFAULT false`);
@@ -12,9 +12,9 @@ export class Updatenoindex1700456789000 {
         await queryRunner.query(`UPDATE "user" SET "noindex" = NOT "noindex" WHERE "noindex" IS NULL`);
     }
 
-//    async down(queryRunner) {
-//        await queryRunner.query(`UPDATE "user" SET "noindex" = NOT "noindex"`);
+//   async down(queryRunner) {
+//        await queryRunner.query(`UPDATE "user" SET "noindex" = NOT "noindex" WHERE "noindex" IS NULL`);
 //        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "noindex" SET DEFAULT true`);
-//        await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "noindex" TO "noindex"`);
+//        await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "noindex" TO "isIndexable"`);
 //    }
 }
