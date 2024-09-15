acf7195be20f411bbbc57941f0255c833343441a
Miriel <me@mri.im>
2024-09-15 10:02:01 +0900

    FIx: d.ts

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -3295,7 +3295,7 @@ export interface Locale extends ILocale {
      */
     "reportAbuseOf": ParameterizedString<"name">;
     /**
-     * 通報理由の詳細を記入してください。対象のノートやPageなどがある場合はそのURLも記入してください。
+     * 通報理由の詳細を記入してください。対象のノートやページなどがある場合はそのURLも記入してください。
      */
     "fillAbuseReportDescription": string;
     /**
@@ -11116,18 +11116,6 @@ export interface Locale extends ILocale {
          */
         "archiveSubscriptionPlan": string;
         /**
-         * ページを削除
-         */
-        "deletePage": string;
-        /**
-         * Playを削除
-         */
-        "deleteFlash": string;
-        /**
-         * ギャラリーの投稿を削除
-         */
-        "deleteGalleryPost": string;
-        /**
          * SystemWebhookを作成
          */
         "createSystemWebhook": string;
@@ -11155,6 +11143,18 @@ export interface Locale extends ILocale {
          * アカウントを削除
          */
         "deleteAccount": string;
+        /**
+         * ページを削除
+         */
+        "deletePage": string;
+        /**
+         * Playを削除
+         */
+        "deleteFlash": string;
+        /**
+         * ギャラリーの投稿を削除
+         */
+        "deleteGalleryPost": string;
     };
     "_fileViewer": {
         /**
